4bc0c8e89d60b18c15c16253d1664951536577ca
Jon Chen <bsd@voltaire.sh>
2020-10-20 08:17:14 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bsdlp/mcping
+
+go 1.15
---
+++ mcping.go
@@ -0,0 +1,26 @@
+package mcping
+
+import (
+	"context"
+	"net"
+	"strconv"
+)
+
+// ResolveMinecraftHostPort looks up srv record
+func ResolveMinecraftHostPort(ctx context.Context, resolver *net.Resolver, server string) (servers []string, err error) {
+	_, addrs, err := resolver.LookupSRV(ctx, "minecraft", "tcp", server)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(addrs) == 0 {
+		return []string{server}, nil
+	}
+
+	servers = make([]string, len(addrs))
+	for i := range servers {
+		port := strconv.FormatUint(uint64(addrs[i].Port), 10)
+		servers[i] = addrs[i].Target + ":" + port
+	}
+	return servers, nil
+}
