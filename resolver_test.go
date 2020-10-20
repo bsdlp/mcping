4772aeedf7941cbe0fcce7a258519e9c641fb0ed
Jon Chen <bsd@voltaire.sh>
2020-10-20 08:38:49 +0000

    tests

--- resolver_test.go
+++ resolver_test.go
@@ -12,9 +12,24 @@ type ResolverTests struct {
 }
 
 func (suite *ResolverTests) TestNXDomain() {
-	hostport, err := ResolveMinecraftHostPort(context.Background(), nil, "doesnotexist.example.com")
+	hostports, err := ResolveMinecraftHostPort(context.Background(), nil, "doesnotexist.example.com")
+	suite.Assert().Error(err)
+	suite.Assert().Empty(hostports)
+}
+
+func (suite *ResolverTests) TestSingleRecord() {
+	hostports, err := ResolveMinecraftHostPort(context.Background(), nil, "single.bsdlp.dev")
 	suite.Assert().NoError(err)
-	suite.Assert().Empty(hostport)
+	suite.Assert().Len(hostports, 1)
+	suite.Assert().Equal("mc.tonkat.su.:25565", hostports[0])
+}
+
+func (suite *ResolverTests) TestMultipleRecord() {
+	hostports, err := ResolveMinecraftHostPort(context.Background(), nil, "multi.bsdlp.dev")
+	suite.Assert().NoError(err)
+	suite.Assert().Len(hostports, 2)
+	suite.Assert().Equal("mc.tonkat.su.:25565", hostports[0])
+	suite.Assert().Equal("mc.tonkat.su.:25566", hostports[1])
 }
 
 func TestResolver(t *testing.T) {
